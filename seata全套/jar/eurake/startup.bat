390e14d59bab01a98791597cd0163d2187985144
lilun <1547709975@qq.com>
2020-08-03 23:09:46 +0800

    全套

---
+++ seata全套/jar/eurake/startup.bat
@@ -0,0 +1,2 @@
+title eureka:8761
+java -Dfile.encoding=utf-8 -jar eureka-1.1.0.jar
---
+++ seata全套/sentinel-dashboard/startup.bat
@@ -0,0 +1,2 @@
+title sentinel-dashboard:8003
+java -Dfile.encoding=utf-8 -jar sentinel-dashboard-1.6.0.jar --server.port=8003
